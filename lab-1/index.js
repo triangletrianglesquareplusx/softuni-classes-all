e5bf5d16fe1406e7da1a1d604b0ed7564c4d2d5a
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-04-13 16:14:22 +0300

    first task done

--- lab-1/index.js
+++ lab-1/index.js
@@ -1,2 +1,16 @@
 'use strict'
-console.log('hey');
+
+class Person{
+    constructor(firstName, lastName, age, email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+    toString(){
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+let mike = new Person('Mike', 'Stephenson', 39, 'mike@helo.com');
+console.log(mike.toString());
